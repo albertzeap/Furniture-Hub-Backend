97f08ec9702d6590d0b60ad5e29687ad18c30859
Albert <albertzeap@gmail.com>
2023-06-27 10:02:58 -0700

    Added first round of user tests

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -1,20 +1,25 @@
 package com.cognixia.jump.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cognixia.jump.repository.UserRepo;
+import com.cognixia.jump.model.User;
+import com.cognixia.jump.service.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 	
 	@Autowired
-	UserRepo userRepo;
+	UserService userService;
 	
 	@GetMapping("/user")
 	public ResponseEntity<?> getUsers(){
@@ -24,8 +29,18 @@ public class UserController {
 		return ResponseEntity.status(200).body(null);
 	}
 	
+	@GetMapping("/user/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable int id){
+		
+		// TODO
+		
+		return ResponseEntity.status(200).body(null);
+	}
+	
+	
+	
 	@PostMapping("/user")
-	public ResponseEntity<?> createUser(){
+	public ResponseEntity<?> createUser(@Valid @RequestBody User user){
 		
 		//TODO
 		
